d5485194645b0c6ca310329d3404210e540c72cb
Jaime Garcia Luna <jluna@gamma18.aulas.gsyc.urjc.es>
2016-09-26 17:18:23 +0200

    Ejercicio4 completo

--- calcoo.py
+++ calcoo.py
@@ -3,29 +3,27 @@
 
 import sys
 
+
 class Calculadora():
-	def suma (self, op1, op2):
-		return op1 + op2
-	def resta (self, op1, op2):
-		return op1 - op2
+    def suma(self, op1, op2):
+        return op1 + op2
+
+    def resta(self, op1, op2):
+        return op1 - op2
 
 
-if __name__=="__main__":
-	try:
-		operando1 = int(sys.argv[1])
-		operando2 = int(sys.argv[3])
-	except ValueError:
-		sys.exit("Error: Non numerical parameters")
-		
-	calculadora = Calculadora()
+if __name__ == "__main__":
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")	
+    calculadora = Calculadora()
 
-	if sys.argv[2] == "suma":
-		result = calculadora.suma(operando1,operando2)
-	elif sys.argv[2] == "resta":
-		result = calculadora.resta(operando1, operando2)
-	else:
-		sys.exit ('operacion solo puede ser suma o resta')
-
-
-	print (result)
-
+    if sys.argv[2] == "suma":
+        result = calculadora.suma(operando1, operando2)
+    elif sys.argv[2] == "resta":
+        result = calculadora.resta(operando1, operando2)
+    else:
+        sys.exit('operacion solo puede ser suma o resta')
+    print(result)
