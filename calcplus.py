44f6749deb88fe52c04f0ef3399d0676e9865ca5
Jaime Garcia Luna <jluna@gamma18.aulas.gsyc.urjc.es>
2016-09-26 16:15:01 +0200

    Ejercicio 6 completo

---
+++ calcplus.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import calcoohija
+
+fichero = open('ficherooperaciones', 'r')
+fichero = fichero.readlines()
+
+
+
+if __name__ == "__main__":
+
+	calcplus = calcoohija.CalculadoraHija()
+
+	for linea in fichero:
+		operaciones = linea.split( ',' )[0]
+		datos = linea.split( ',' )[1:]
+		result = int(datos[0])
+
+		if operaciones == "suma":
+			print ("Resultado suma = ")
+			for n in range (1, len(datos)):
+				result = calcplus.suma(result, int(datos[n]))
+		elif operaciones == "resta":
+			print ("Resultado resta = ")
+			for n in range (1, len(datos)):
+				result = calcplus.resta(result, int(datos[n]))
+		elif operaciones == "multiplicacion":
+			print ("Resultado multiplicacion = ")
+			for n in range (1, len(datos)):
+				result = calcplus.multi(result, int(datos[n]))
+		elif operaciones == ("division"):
+			print ("Resultado division = ")
+			for n in range (1, len(datos)):
+				if datos == "0":
+					result = ("Error: Division by 0 is not allowed")
+				else:
+					result = calcplus.divi(result, int(datos[n]))
+		else: 
+			sys.exit ('operacion solo puede ser suma, resta, multiplicacion o division')
+	
+		print (result)
+
+
+												
+	
+
+
+												
+
+
