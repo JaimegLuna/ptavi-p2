188de9e67bc3ceb93b706378ef0d5355eaf0407d
prodaromero <elrodas95@gmail.com>
2017-10-02 20:25:04 +0200

    calcplusplus.py pep8 pasado

--- calcplusplus.py
+++ calcplusplus.py
@@ -7,38 +7,36 @@ import csv
 
 fichero = sys.argv[1]
 with open(fichero) as fich:
-	fichero = csv.reader(fich)
-	calcplus = calcoohija.CalculadoraHija()
+    fichero = csv.reader(fich)
+    calcplus = calcoohija.CalculadoraHija()
 
-	for line in fichero:
-		operations = line[0]
-		datos = line[1:]
-		result = int(datos[0])
+    for line in fichero:
+        operations = line[0]
+        datos = line[1:]
+        result = int(datos[0])
 
+        if operations == "multiplicación":
+            print("resultado multiplicación = ")
+            for n in range(1, len(datos)):
+                resultado = calcplus.multi(resultado, int(datos[n]))
 
+        elif operations == "división":
+            print("resultado división = ")
+            for n in range(1, len(datos)):
+                if datos == "0":
+                    resultado = ("Error: no se puede dividir por 0")
+                else:
+                    resultado = calcplus.div(resultado, int(datos[n]))
+        elif operations == "suma":
+            print("resultado suma = ")
+            for n in range(1, len(datos)):
+                resultado = calcplus.suma(resultado, int(datos[n]))
 
-		if operations == "multiplicación":
-			print("resultado multiplicación = ")
-			for n in range (1, len(datos)):
-				resultado = calcplus.multi(resultado, int(datos[n]))
+        elif operations == "resta":
+            print("resultado resta = ")
+            for n in range(1, len(datos)):
+                resultado = calcplus.rest(resultado, int(datos[n]))
+        else:
+            sys.exit("sólo puede ser multiplicación, división, suma o resta")
 
-		elif operations == "división":	
-			print("resultado división = ")
-			for n in range (1, len(datos)):
-				if datos == "0":
-					resultado = ("Error: no se puede dividir por 0")
-				else:
-					resultado = calcplus.div(resultado, int(datos[n]))
-		elif operations == "suma":
-			print("resultado suma = ")
-			for n in range(1, len(datos)):
-				resultado = calcplus.suma(resultado, int(datos[n])) 
-
-		elif operations == "resta":
-			print("resultado resta = ")
-			for n in range (1, len(datos)):
-				resultado = calcplus.rest(resultado, int(datos[n]))
-		else:
-			sys.exit("sólo puede ser multiplicación, división, suma o resta")
-
-		print(resultado)
+        print(resultado)
