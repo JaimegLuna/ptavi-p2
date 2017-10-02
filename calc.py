b02594b3104e308a4df1e304dfac05794a00f635
prodaromero <elrodas95@gmail.com>
2017-10-02 20:06:12 +0200

    Calc.py Pep8 pasado

--- calc.py
+++ calc.py
@@ -3,27 +3,28 @@
 
 import sys
 
+
 def suma(op1, op2):
-	"""Función que suma los operandos"""
-	return op1 + op2
+    """Función que suma los operandos"""
+    return op1 + op2
+
 
 def rest(op1, op2):
-	"""Función que resta los operandos"""
-	return op1 - op2
+    """Función que resta los operandos"""
+    return op1 - op2
 
 
-if __name__=="__main__":
-	try:
+if __name__ == "__main__":
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Non numerical parameters")
 
-		operando1 = int(sys.argv[1])
-		operando2 = int(sys.argv[3])
-	except ValueError: 
-		sys.exit("Non numerical parameters")
-	
-	if sys.argv[2] == "suma": 
-		resultado = suma(operando1, operando2)
-	elif sys.argv[2] == "resta":
-		resultado = rest(operando1, operando2)
-	else:
-		sys.exit("Sólo puede ser suma o resta")
-	print(resultado)
+    if sys.argv[2] == "suma":
+        resultado = suma(operando1, operando2)
+    elif sys.argv[2] == "resta":
+        resultado = rest(operando1, operando2)
+    else:
+        sys.exit("Sólo puede ser suma o resta")
+    print(resultado)
