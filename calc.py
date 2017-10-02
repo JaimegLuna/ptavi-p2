d1d4843f26a30606c16d6728d6ca58f450be6064
Jaime Garcia Luna <jluna@gamma36.aulas.gsyc.urjc.es>
2017-10-02 11:14:11 +0200

    calc.py sin pep 8

--- calc.py
+++ calc.py
@@ -3,28 +3,27 @@
 
 import sys
 
+def suma(op1, op2):
+	"""Función que suma los operandos"""
+	return op1 + op2
 
-def plus(op1, op2):
-    """ Function to sum the operands """
-    return op1 + op2
+def rest(op1, op2):
+	"""Función que resta los operandos"""
+	return op1 - op2
 
 
-def minus(op1, op2):
-    """ Function to substract the operands """
-    return op1 - op2
+if __name__=="__main__":
+	try:
 
-if __name__ == "__main__":
-    try:
-        operando1 = int(sys.argv[1])
-        operando2 = int(sys.argv[3])
-    except ValueError:
-        sys.exit("Error: Non numerical parameters")
-
-    if sys.argv[2] == "suma":
-        result = plus(operando1, operando2)
-    elif sys.argv[2] == "resta":
-        result = minus(operando1, operando2)
-    else:
-        sys.exit('Operación sólo puede ser sumar o restar.')
-
-    print(result)
+		operando1 = int(sys.argv[1])
+		operando2 = int(sys.argv[3])
+	except ValueError: 
+		sys.exit("Non numerical parameters")
+	
+	if sys.argv[2] == "suma": 
+		resultado = suma(operando1, operando2)
+	elif sys.argv[2] == "resta":
+		resultado = rest(operando1, operando2)
+	else:
+		sys.exit("Sólo puede ser suma o resta")
+	print(resultado)
